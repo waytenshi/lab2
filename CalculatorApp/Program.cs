dabe0462ac2810f45fb623eb3a089a2ff274c974
waytenshi <ddiimmaa567@gmail.com>
2025-02-17 00:36:18 +0300

    Initial commit: created console menu

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Меню выбора:");
+        Console.WriteLine("1 - Ввести A");
+        Console.WriteLine("2 - Ввести B");
+        Console.WriteLine("3 - Выполнить операцию “+”");
+        Console.WriteLine("4 - Выполнить операцию “-”");
+        Console.WriteLine("5 - Выполнить операцию “*”");
+        Console.WriteLine("6 - Выполнить операцию “/”");
+
+        Console.ReadKey();
+    }
+}
