87c7b057e79ba8caf27ca6e2a560f026db6a272c
waytenshi <ddiimmaa567@gmail.com>
2025-02-17 00:52:57 +0300

    Added input for A

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -4,14 +4,57 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Меню выбора:");
-        Console.WriteLine("1 - Ввести A");
-        Console.WriteLine("2 - Ввести B");
-        Console.WriteLine("3 - Выполнить операцию “+”");
-        Console.WriteLine("4 - Выполнить операцию “-”");
-        Console.WriteLine("5 - Выполнить операцию “*”");
-        Console.WriteLine("6 - Выполнить операцию “/”");
+        double A = 0, B = 0;
 
-        Console.ReadKey();
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Меню:");
+            Console.WriteLine("1. Ввести A");
+            Console.WriteLine("2. Ввести B");
+            Console.WriteLine("3. Выполнить операцию '+'");
+            Console.WriteLine("4. Выполнить операцию '-'");
+            Console.WriteLine("5. Выполнить операцию '*'");
+            Console.WriteLine("6. Выполнить операцию '/'");
+            Console.WriteLine("0. Выход");
+
+            Console.Write("Выберите действие: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Введите значение для A: ");
+                    A = Convert.ToDouble(Console.ReadLine());
+                    break;
+                case "2":
+                    Console.Write("Введите значение для B: ");
+                    B = Convert.ToDouble(Console.ReadLine());
+                    break;
+                case "3":
+                    Console.WriteLine($"A + B = {A + B}");
+                    break;
+                case "4":
+                    Console.WriteLine($"A - B = {A - B}");
+                    break;
+                case "5":
+                    Console.WriteLine($"A * B = {A * B}");
+                    break;
+                case "6":
+                    if (B != 0)
+                        Console.WriteLine($"A / B = {A / B}");
+                    else
+                        Console.WriteLine("Ошибка: деление на ноль!");
+                    break;
+                case "0":
+                    return;
+                default:
+                    Console.WriteLine("Неверный выбор!");
+                    break;
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
     }
 }
